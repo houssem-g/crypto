b2c21c776f7f2a1c03a408fc0d0ee0845adaac61
houssem-g <houssem.gharsalli@hotmail.fr>
2021-09-21 22:04:05 +0200

    django setup

--- backend/api_get_data.py
+++ backend/api_get_data.py
@@ -1,2 +1,3 @@
 # https://www.botreetechnologies.com/blog/monitoring-cryptocurrency-markets-cryptocompare-python-api-client/
 # https://pypi.org/project/cryptocompare/
+# https://www.univ-orleans.fr/iut-orleans/informatique/intra/tuto/django/Tuto-Django.pdf
