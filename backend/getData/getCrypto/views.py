2d4e9eafd516d464c927b1f1bd7c594d11af3b89
houssem-g <houssem.gharsalli@hotmail.fr>
2021-10-04 20:36:47 +0200

    link react and django via axios

--- backend/getData/getCrypto/views.py
+++ backend/getData/getCrypto/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
-from django.template import Template,Context
-from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import Template,Context
+# from django.shortcuts import render
 import requests
+# check how JsonResponse work
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -9,9 +11,13 @@ def home(request):
     # objets=Crypto.objects.all()
     objets = get_crypto_data()
     # print(objets)
-    template=Template('{% for elem in objets %} {{elem}} <br />{%endfor%}')
-    context=Context({'objets':objets})
-    return HttpResponse(template.render(context))
+    # template=Template('{% for elem in objets %} {{elem}} <br />{%endfor%}')
+    # context=Context({'objets':objets})
+    # return HttpResponse(template.render(context))
+    
+    # to return an array we need to use JsonResponse
+    return JsonResponse(objets, safe=False)
+
 
 
 def get_crypto_data():
