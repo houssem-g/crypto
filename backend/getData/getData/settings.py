0e9810fa10955d4650f25d81bb9f7b085ee27037
houssem-g <houssem.gharsalli@hotmail.fr>
2022-03-03 08:52:01 +0100

    update settings django for deployment

--- backend/getData/getData/settings.py
+++ backend/getData/getData/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k18ng1b3y@&+=hc_n@-xo49ajx-uqdt2ihl=w@y_d2%5pa^d#c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "crypto-tracking.azurewebsites.net"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,6 +145,8 @@ DATE_INPUT_FORMAT = ('%d/%m/%Y', '%Y-%m-%d')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
